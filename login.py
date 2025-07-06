11bc1aecf40e2f5a728745bcad4dc748239bd79e
olli627 <2816014718@qq.com>
2025-07-06 15:03:00 +0800

    update

--- login.py
+++ login.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 driver = webdriver.Edge()
 driver.get("http://webdriveruniversity.com/")
 driver.maximize_window()
-driver.get_screenshot_as_png()
 
 sleep(5)
 driver.quit()
